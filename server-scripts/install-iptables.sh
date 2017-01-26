fe6bef733e32ce973fbc27a256586096afa38bdb
Carlos <cdelatorre@correo.ugr.es>
2017-01-26 18:57:12 +0100

    Creando estructura de script para iptables

---
+++ server-scripts/install-iptables.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+# Titulo:       Instalación y configuración de IpTables
+# Fecha:        26/01/17
+# Autor:        Carlos de la Torre
+# Versión:      1.0
+# Descripción: Instalación y configuración de las diferentes reglas de IpTables, junto con los paquetes necesarios para que los servicios y las utilidades
+# Opciones: Ninguna
+# Uso: install-iptables.sh
+
+
+# VARIABLES ESTATICAS
+DEPENDS=(ifconfig nmap find git) # Dependencias necesarias
+DIRS=(/usr/bin /usr/sbin /bin) # Directorios de busqueda
+MASKS_CIDR=(32 31 30 29 28 27 26 25 24 23 22)
+MASKS_DEC=(255.255.255.255 255.255.255.254 255.255.255.252 255.255.255.248 255.255.255.240 255.255.255.224 255.255.255.192 255.255.255.128 255.255.255.0 255.255.254.0 255.255.252.0)
+
+# VARIABLES GLOBALES
+TEST_IP=62.15.168.50
+TEMP_FILE=/tmp/file.tmp
+
+# FUNCIONES
+
+# Función para deter la ejecución con o sin mensaje
+# parámetro de entrada $1 == --with-msg para poner mensaje (opcional)
+# #parámetro de entrada $2 == cadena de texto con el mensaje (opcional)
+function pause() {
+    if [[ $1 == "--with-msg" ]] && [[ $2 != "" ]]; then
+        read -p "$2";
+    elif [[ $1 == "--with-msg" ]]; then
+        read -p "Presione Enter para continuar o Ctrl+C para cancelar.";
+    else
+        read -s -t 5
+    fi
+}
+
+# Función para saber si se esta utilizando root
+function is_root {
+    if [[ "$(whoami)" != "root" ]]; then
+        echo
+        echo "  Tiene que ejecutar este script con permisos de administrador";
+        echo
+        exit;
+    fi
+}
+
+function create_user() {
+    echo "Indique el usuario que desea crear"
+    pause --with-msg;
+}
+
+# Funcion de wrapper para find
+# parámetro de entrada $1 lo que queremos buscar
+function buscar(){
+    if [ $1 != "" ]; then
+        local retval=$(find / -name $1)
+    else
+        echo "Faltán parámetros para buscar"
+    fi
+    echo $retval
+}
+
+# Función que se encarga de buscar las dependencias
+function check_depends() {
+    for depend in ${DEPENDS[@]}
+    do
+        hit=0
+        for dir in ${DIRS[@]}
+        do
+            if [ -x "$dir/$depend" ]; then
+                echo "Dependencia encontrada: $depend"
+                hit=1
+                break;
+            fi
+        done
+        if [ $hit == 0 ]; then
+            echo "Dependencia NO encontrada: $depend"
+        fi
+    done
+}
+
+
+# Función para presentar el Menú
+# Sin parámetros de entrada
+function menu() {
+    clear;
+    echo
+    echo "           ****************************************"
+    echo "           *          Esto es el Menú             *"
+    echo "           * 1.-                                  *"
+    echo "           * 2.-                                  *"
+    echo "           * 3.-                                  *"
+    echo "           * 4.-                                  *"
+    echo "           * 5.-                                  *"
+    echo "           * 6.-                                  *"
+    echo "           * 7.-                                  *"
+    echo "           * 8.-                                  *"
+    echo "           *                                      *"
+    echo "           * 0.- Salir                            *"
+    echo "           ****************************************"
+    echo
+    read -p "           Elija una opción: " option
+    case $option in
+        0)
+        exit;
+        ;;
+        1)
+
+        pause;
+        menu;
+        ;;
+        2)
+
+        pause;
+        menu;
+        ;;
+        3)
+
+        pause;
+        menu;
+        ;;
+        4)
+
+        pause;
+        menu;
+        ;;
+        5)
+
+        pause;
+        menu;
+        ;;
+        6)
+
+        pause;
+        menu;
+        ;;
+        7)
+
+        pause;
+        menu;
+        ;;
+        8)
+
+        pause;
+        menu;
+        ;;
+        *)
+        echo "Opción no permitida";
+        pause;
+        menu;
+        ;;
+    esac
+}
+is_root;
+menu;
